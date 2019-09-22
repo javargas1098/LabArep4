fa8f0b7be0e15d0d6da10f15b38ec64f7dc7b210
Javier Vargas <ja.vargas1098@gmail.com>
2019-09-21 22:45:52 -0500

    falta articulo

--- ClientAWS/src/main/java/groupId/ClientAWS/URLReader.java
+++ ClientAWS/src/main/java/groupId/ClientAWS/URLReader.java
@@ -8,17 +8,17 @@ import java.util.logging.Logger;
 public class URLReader implements Runnable {
 
 	private URL url;
-	private int i;
+	private int x;
 
-	public URLReader(URL url, int i) {
+	public URLReader(URL url, int x) {
 		this.url = url;
-		this.i = i;
+		this.x = x;
 	}
 
 	@Override
 	public void run() {
 		try {
-			File archivo = new File("./rta" + i + ".html");
+			File archivo = new File("./rta" + x + ".html");
 			BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
 			try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
 				String inputLine = null;
@@ -27,8 +27,8 @@ public class URLReader implements Runnable {
 					bw.write(inputLine);
 				}
 				bw.close();
-			} catch (IOException x) {
-				Logger.getLogger(URLClient.class.getName()).log(Level.SEVERE, null, x);
+			} catch (IOException e) {
+				Logger.getLogger(URLClient.class.getName()).log(Level.SEVERE, null, e);
 			}
 		} catch (IOException ex) {
 			Logger.getLogger(URLReader.class.getName()).log(Level.SEVERE, null, ex);
---
+++ README.md
@@ -0,0 +1,23 @@
+# WebApi  
+   
+## Empezando  
+  
+Para ejecutar el programa solo debe ir al link de heroku `[https://arepweb.herokuapp.com/](webapi)` en el articulo se encuentra explicado como se debe usar el link para poder evideciar el funcionamiento del la apliacion
+
+### Documentación  
+  
+La documentación se encuentra en la carpeta doc.  
+  
+
+## Autor  
+  
+
+* **Javier Vargas** - *ECI*  
+  
+
+## License  
+  
+
+Este proyecto está licenciado bajo GNU  License v3.0 - ver [LICENSE](LICENSE) para más detalles.  
+  
+
